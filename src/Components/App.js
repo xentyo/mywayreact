b49f161c7184a8ce08fadde18d72d81bc9a8f071
Yolanda Monserrat <yolanda.garcia16@tectijuana.edu.mx>
2018-07-02 12:12:50 -0600

    Se hizo la estructura de los componentes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
-import './App.css';
+import './global/css/App.css';
 
 class App extends Component {
   constructor(props) {
---
+++ src/Components/Sign.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './global/css/App.css';
+
+class Sign extends Component {
+    render(){
+
+    }
+}
+
+export default Sign;
