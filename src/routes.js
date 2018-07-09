fa4efaf47026a03e2b080dceee8b31615efb732a
Daniel Medina <daniel.medina@tectijuana.edu.mx>
2018-07-09 11:22:42 -0700

    Added map route

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import { Route, Switch } from "react-router-dom";
 import About from "./Components/About";
 import Sign from "./Components/Sign";
 import Login from "./Components/Login";
+import Map from "./Components/Map";
 
 const AppRoutes = () => (
   <About>
     <Switch>
       <Route exact path="/sign" component={Sign} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/map" component={Map} />
     </Switch>
   </About>
 );
