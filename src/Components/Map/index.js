86fc5e401e034502e8c599337a27c4eb8dc0b4dc
Daniel Medina <daniel.medina@tectijuana.edu.mx>
2018-07-09 12:39:22 -0700

    Get user location

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 const Mapbox = ReactMapboxGl({
   accessToken:
@@ -11,13 +11,49 @@ class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      testPoint: [-0.481747846041145, 51.3233379650232]
+      testPoint: [-0.481747846041145, 51.3233379650232],
+      latitude: null,
+      longitude: null
     };
   }
+
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      this.loadPosition();
+    }
+  }
+
+  loadPosition = async () => {
+    try {
+      const position = await this.getCurrentPosition();
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(position);
+      console.log(position.coords);
+      this.setState({
+        latitude,
+        longitude
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getCurrentPosition = (options = {}) => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
 
   render() {
     return (
       <Container>
+        <Row>
+          <Col>
+            <h1>Lat: {this.state.latitude}</h1>
+            <h1>Long: {this.state.longitude}</h1>
+          </Col>
+        </Row>
         <Mapbox
           // eslint-disable-next-line
           style="mapbox://styles/danielmedina/cjj8ts3l60uny2sovd7aefrwz"
