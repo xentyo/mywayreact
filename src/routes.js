79e0e4cbe137c9b9fca8eb7aad2af6f00efff808
Daniel Medina <isc.daniel.medina@gmail.com>
2018-07-09 10:41:53 -0700

    Delete app.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 //Components
-import About from './Components/About';
-import Sign from './Components/Sign';
-import App from './Components/App';
+import About from "./Components/About";
+import Sign from "./Components/Sign";
+import Login from "./Components/Login";
 
-const AppRoutes = () =>
-    <About>
-        <Switch>
-            <Route exact path="/sign" component={Sign} />
-            <Route exact path="/app" component={App}/>
-        </Switch>
-    </About>;
+const AppRoutes = () => (
+  <About>
+    <Switch>
+      <Route exact path="/sign" component={Sign} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+  </About>
+);
 
 export default AppRoutes;
