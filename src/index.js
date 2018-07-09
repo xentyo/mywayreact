4d1231f178023d46253c36727eb57e586c52dd98
Daniel Medina <isc.daniel.medina@gmail.com>
2018-07-09 10:17:23 -0700

    Merge intento 2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 //Routes
-import registerServiceWorker from './registerServiceWorker';
-<<<<<<< HEAD
-import AppRoutes from './routes';
+import registerServiceWorker from "./registerServiceWorker";
+import AppRoutes from "./routes";
 
 //Assets
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import 'bootstrap/dist/css/bootstrap.min.css'
->>>>>>> development
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 render(
-    <Router>
-        <AppRoutes />
-    </Router>,
-    document.getElementById('root'));
+  <Router>
+    <AppRoutes />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
